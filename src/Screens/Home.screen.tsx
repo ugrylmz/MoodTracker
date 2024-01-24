73360796409e56af8557d31e3f1fa6a80d306733
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-24 23:31:11 +0300

    feat(Home.screen.tsx): add MoodPicker component to Home screen The Home screen now includes the MoodPicker component, allowing users to select their mood. This enhances the functionality and user experience of the Home screen.

--- src/Screens/Home.screen.tsx
+++ src/Screens/Home.screen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import MoodPicker from '../components/MoodPicker';
+import Colors from '../../constants/Colors';
 const Home: React.FC = () => {
   return (
     <View style={styles.container}>
+      <MoodPicker />
     </View>
   );
 };
