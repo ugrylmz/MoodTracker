cd4dc3f20f3cd57e5f3bf0316bc86db1f6e20cf1
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-24 23:30:11 +0300

    feat(App.tsx): add navigation container and bottom tabs navigator The App component now includes a NavigationContainer from the '@react-navigation/native' package, which provides a container for the navigation hierarchy. Additionally, the BottomTabsNavigator component is imported and rendered within the NavigationContainer, allowing for navigation between different screens using a bottom tab navigation pattern.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import BottomTabsNavigator from './Screens/BottomTabs.navigator';
+
 const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <BottomTabsNavigator />
+    </NavigationContainer>
+  );
 };
+
 export default App;
