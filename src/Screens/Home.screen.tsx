999f1815afbcecbdf89b93a24b5dbc02129cca5a
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-26 22:36:59 +0300

    feat(Home.screen.tsx): add state for moodList and handleSelectMood function The Home screen component now includes state for moodList, which is an array of objects containing the selected mood and its timestamp. The handleSelectMood function is also added to handle the selection of a mood and update the moodList state accordingly. Additionally, the selected moods are displayed in the component by mapping over the moodList array and rendering the mood emoji and timestamp.

--- src/Screens/Home.screen.tsx
+++ src/Screens/Home.screen.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import MoodPicker from '../components/MoodPicker';
-import Colors from '../../constants/Colors';
+import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
 const Home: React.FC = () => {
+  const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
+
+  const handleSelectMood = useCallback((mood: MoodOptionType) => {
+    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
+  }, []);
   return (
     <View style={styles.container}>
-      <MoodPicker />
+      <MoodPicker handleSelectMood={handleSelectMood} />
+      {moodList.map(item => (
+        <Text key={item.timestamp}>
+          {item.mood.emoji} {new Date(item.timestamp).toString()}
+        </Text>
+      ))}
     </View>
   );
 };
