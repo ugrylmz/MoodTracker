4947e1dfeeb206eba2b6e623dd658756af25d153
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-29 23:34:57 +0300

    refactor(Home.screen.tsx): remove unused code and optimize imports The unused code related to the moodList state and the MoodItemRow component has been removed. The imports have been optimized to only import necessary dependencies. This improves code readability and reduces unnecessary code clutter.

--- src/Screens/Home.screen.tsx
+++ src/Screens/Home.screen.tsx
@@ -1,20 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React, { FC } from 'react';
 import { View, StyleSheet } from 'react-native';
 import MoodPicker from '../components/MoodPicker';
-import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
-import MoodItemRow from '../components/MoodItemRow';
-const Home: React.FC = () => {
-  const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
+import { useAppContext } from '../App.provider';
 
-  const handleSelectMood = useCallback((mood: MoodOptionType) => {
-    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
-  }, []);
+const Home: FC = () => {
+  const { handleSelectMood } = useAppContext();
+
   return (
     <View style={styles.container}>
       <MoodPicker handleSelectMood={handleSelectMood} />
-      {moodList.map(item => (
-        <MoodItemRow moodItem={item} key={item.timestamp} />
-      ))}
     </View>
   );
 };
