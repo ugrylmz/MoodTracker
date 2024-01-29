ecfbbe07d52eadbc8ef676ba89538d248b607f40
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-29 23:38:02 +0300

    feat(History.screen.tsx): add moodList state and render MoodItemRow component for each item In this commit, the History screen component is updated to include a moodList state variable obtained from the app context. The moodList is then mapped over to render a MoodItemRow component for each item in the list. This allows the History screen to display a list of mood items.

--- src/Screens/History.screen.tsx
+++ src/Screens/History.screen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 import { useAppContext } from '../App.provider';
+import MoodItemRow from '../components/MoodItemRow';
 
 const History: React.FC = () => {
+  const { moodList } = useAppContext();
+  console.log('moodList', moodList);
   return (
     <View>
+      {moodList.map(item => (
+        <MoodItemRow moodItem={item} key={item.timestamp} />
+      ))}
     </View>
   );
 };
