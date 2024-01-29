335bdd8361e5fe542809fc2a922e6d6b73b89c95
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-29 23:37:49 +0300

    refactor(History.screen.tsx): remove unused import and variable The import statement for the Text component from 'react-native' and the appContext variable are no longer used in the History screen. This commit removes the unused import and variable to improve code cleanliness and readability.

--- src/Screens/History.screen.tsx
+++ src/Screens/History.screen.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { useAppContext } from '../App.provider';
 
 const History: React.FC = () => {
-  const appContext = useAppContext();
   return (
     <View>
-      <Text>{appContext.greeting}</Text>
     </View>
   );
 };
