39619bd417b5f26e4f448471c615ee4f5010d8d5
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-31 00:39:34 +0300

    refactor(Home.screen.tsx): import Image and ImageBackground from react-native feat(Home.screen.tsx): add background image to the Home screen The import statements have been updated to include Image and ImageBackground from react-native. Additionally, a background image has been added to the Home screen using the ImageBackground component. The image is sourced from a URL and displayed within the container view.

--- src/Screens/Home.screen.tsx
+++ src/Screens/Home.screen.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, Image, ImageBackground } from 'react-native';
 import MoodPicker from '../components/MoodPicker';
 import { useAppContext } from '../App.provider';
 
 const Home: FC = () => {
   const { handleSelectMood } = useAppContext();
 
+  const imageUrl =
+    'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
+
   return (
-    <View style={styles.container}>
+    <ImageBackground style={styles.container} source={{uri: imageUrl}}>
       <MoodPicker handleSelectMood={handleSelectMood} />
-    </View>
+    </ImageBackground>
   );
 };
 
