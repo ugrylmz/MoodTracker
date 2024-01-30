bbbb14ffc08d9e2fa2c911119d36311c3bdeefea
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-01-31 00:37:52 +0300

    refactor(App.provider.tsx): improve code readability and add data persistence feat(App.provider.tsx): add useEffect hook to fetch app data on component mount and update moodList state The code in App.provider.tsx has been refactored to improve code readability. The import statements have been formatted for better readability. The handleSelectMood function has been modified to update the moodList state and persist the data using the setAppData function. Additionally, an useEffect hook has been added to fetch app data on component mount and update the moodList state. This ensures that the app data is fetched and displayed correctly when the component is rendered.

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useCallback, useContext, useState } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { MoodOptionType, MoodOptionWithTimestamp } from './types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -41,16 +47,30 @@ export const AppProvider: React.FC<React.PropsWithChildren<{}>> = ({
 }) => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
 
+  useEffect(() => {
+    const fetchAppData = async () => {
+      const data = await getAppData();
+      if (data) {
+        setMoodList(data.moodList);
+      }
+    };
+    fetchAppData();
+  }, []);
+
   const handleSelectMood = useCallback(
-    (mood: MoodOptionType, note?: String) => {
-      setMoodList(current => [
-        ...current,
-        {
-          moodOption: mood,
-          timestamp: Date.now(),
-          note: note, // Provide a value for the 'note' property
-        } as MoodOptionWithTimestamp,
-      ]);
+    (selectedMood: MoodOptionType, note?: string) => {
+      setMoodList(current => {
+        const newMoodList = [
+          ...current,
+          {
+            mood: selectedMood,
+            timestamp: Date.now(),
+            note: note || '',
+          },
+        ];
+        setAppData({ moodList: newMoodList });
+        return newMoodList;
+      });
     },
     [],
   );
