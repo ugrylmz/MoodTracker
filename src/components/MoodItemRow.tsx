2a4b9d3cfe76b7b81850e6dfe65e1c12b4f5651c
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-02-01 00:36:05 +0300

    refactor(MoodItemRow.tsx): optimize code structure and add swipe-to-delete functionality feat(MoodItemRow.tsx): implement swipe-to-delete functionality using react-native-gesture-handler and react-native-reanimated libraries The code in MoodItemRow.tsx has been refactored to improve code structure and readability. The swipe-to-delete functionality has been added using the react-native-gesture-handler and react-native-reanimated libraries. Users can now swipe left on a mood item to delete it.
    style(MoodItemRow.tsx): decrease font size of text and moodDescription to 14
    style(MoodItemRow.tsx): add DeleteText style with color and font size
    The font size of the text and moodDescription in the MoodItemRow component has been decreased to 14 for better visual consistency. Additionally, a new style called DeleteText has been added with a color of theme.colorPurple and a font size of 12.

--- src/components/MoodItemRow.tsx
+++ src/components/MoodItemRow.tsx
@@ -1,31 +1,90 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useCallback } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  LayoutAnimation,
+} from 'react-native';
 import { format } from 'date-fns';
 import { MoodOptionWithTimestamp } from '../types';
 import { theme } from '../theme';
+import { useAppContext } from '../App.provider';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Reanimated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 type MoodItemRowProps = {
   moodItem: MoodOptionWithTimestamp;
 };
 
+const maxPan = 80;
+
 const MoodItemRow: React.FC<MoodItemRowProps> = ({ moodItem }) => {
+  const { handleDeleteMood } = useAppContext();
+  const offset = useSharedValue(0);
+
+  const handleDelete = useCallback(() => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    handleDeleteMood(moodItem);
+  }, [handleDeleteMood, moodItem]);
+
+  const removeWithDelay = () => {
+    setTimeout(() => {
+      handleDeleteMood(moodItem);
+    }, 250);
+  };
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }));
+
+  const panGesture = Gesture.Pan()
+    .onUpdate(event => {
+      const xVal = Math.floor(event.translationX);
+      offset.value = xVal;
+
+      if (Math.abs(xVal) > maxPan) {
+        offset.value = withTiming(Math.sign(offset.value) * 2000, {}, () =>
+          runOnJS(removeWithDelay)(),
+        );
+      }
+    })
+    .onEnd(() => {
+      offset.value = withTiming(0);
+    });
+
   return (
-    <View style={styles.container}>
-      <View style={styles.moodItem}>
-        <View style={styles.iconAndDescription}>
-          <Text style={styles.moodValue}>{moodItem?.mood?.emoji} </Text>
-          <Text style={styles.moodDescription}>
-            {moodItem?.mood?.description}
+    <GestureDetector gesture={panGesture}>
+      <Reanimated.View style={animatedStyle}>
+        <View style={styles.container}>
+          <View style={styles.moodItem}>
+            <View style={styles.iconAndDescription}>
+              <Text style={styles.moodValue}>{moodItem?.mood?.emoji} </Text>
+              <Text style={styles.moodDescription}>
+                {moodItem?.mood?.description}
+              </Text>
+            </View>
+            <Text style={styles.moodDate}>
+              {format(
+                new Date(moodItem.timestamp),
+                "dd MMM, yyyy 'at' hh:mm:ss",
+              )}
+            </Text>
+            <Pressable onPress={handleDelete}>
+              <Text style={styles.DeleteText}>Delete</Text>
+            </Pressable>
+          </View>
+          <Text style={styles.text}>
+            Notes: {moodItem?.note ? moodItem.note : ' '}
           </Text>
         </View>
-        <Text style={styles.moodDate}>
-          {format(new Date(moodItem.timestamp), "dd MMM, yyyy 'at' hh:mm:ss")}
-        </Text>
-      </View>
-      <Text style={styles.text}>
-        Notes: {moodItem?.note ? moodItem.note : ' '}
-      </Text>
-    </View>
+      </Reanimated.View>
+    </GestureDetector>
   );
 };
 
@@ -35,7 +94,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   text: {
-    fontSize: 18,
+    fontSize: 14,
     color: theme.colorPurple,
     fontWeight: 'bold',
     marginLeft: 10,
@@ -58,13 +117,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   moodDescription: {
-    fontSize: 18,
+    fontSize: 14,
     color: theme.colorPurple,
     fontWeight: 'bold',
   },
   iconAndDescription: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  DeleteText: {
+    color: theme.colorPurple,
+    fontSize: 12,
   },
 });
 
