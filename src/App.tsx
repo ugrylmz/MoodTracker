825925f5fe8f625f69ffb1b9ae3117018fc9cba8
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-02-01 00:32:07 +0300

    chore(App.tsx): enable layout animation on Android The code has been updated to enable layout animation on Android devices. This is done by checking the platform OS and if it is Android, enabling the layout animation using the `setLayoutAnimationEnabledExperimental` method from the `UIManager` module. This change improves the user experience on Android devices by allowing smooth and animated transitions.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import BottomTabsNavigator from './Screens/BottomTabs.navigator';
 import { AppProvider } from './App.provider';
+import { Platform, UIManager } from 'react-native';
+
+if (Platform.OS === 'android') {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
 
 const App: React.FC = () => {
   return (
