11afb8d4a1108fc6f9248a9c07fcacd6015e9dc3
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-02-03 01:12:05 +0300

    refactor(App.tsx): add useEffect hook to hide SplashScreen The useEffect hook is added to the App component to hide the SplashScreen when the component mounts. This improves the user experience by preventing the SplashScreen from being displayed for an extended period of time.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import BottomTabsNavigator from './Screens/BottomTabs.navigator';
 import { AppProvider } from './App.provider';
 import { Platform, UIManager } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -11,6 +12,9 @@ if (Platform.OS === 'android') {
 }
 
 const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <AppProvider>
       <NavigationContainer>
